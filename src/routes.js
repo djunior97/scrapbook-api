aa612f9a85767ebb05c090af2a908ab767a1d30f
dirceuAntunes <dirceu.antunes@garupa.co>
2020-10-09 10:39:23 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import cors from 'cors'
+
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+import ScrapController from './app/controllers/ScrapController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+routes.use(cors());
+
+routes.post('/users', UserController.store);
+
+routes.post('/login', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.get('/users', UserController.index);
+routes.put('/users', UserController.update);
+routes.delete('/users', UserController.delete);
+
+routes.get('/scraps', ScrapController.index);
+routes.post('/scraps', ScrapController.store);
+routes.put('/scraps/:id', ScrapController.update);
+routes.delete('/scraps/:id', ScrapController.delete);
+
+export default routes;
